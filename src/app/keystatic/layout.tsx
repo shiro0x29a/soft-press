60c71db52e769676f89a06b52fa6ff462e385807
anik <anik@anik.anik>
2026-04-06 05:08:41 +0300

    _

--- src/app/keystatic/layout.tsx
+++ src/app/keystatic/layout.tsx
@@ -3,5 +3,17 @@ import KeystaticApp from "./keystatic";
 export const dynamic = "force-dynamic";
 
 export default function RootLayout() {
-  return <KeystaticApp />;
+  return (
+    <div
+      style={{
+        position: "fixed",
+        inset: 0,
+        zIndex: 99999,
+        backgroundColor: "var(--background)",
+        overflow: "auto",
+      }}
+    >
+      <KeystaticApp />
+    </div>
+  );
 }
